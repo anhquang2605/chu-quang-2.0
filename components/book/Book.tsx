0a4e934658dbe5f73cd9d2e17a55e2015f6bdbbf
Anh Quang Chu <anhquang2605@gmail.com>
2025-07-01 13:18:51 -0700

    removed commment

--- components/book/Book.tsx
+++ components/book/Book.tsx
@@ -80,25 +80,6 @@ const Page: React.FC<PageProps> = () => {
     mesh.bind(skeleton);
     return mesh;
   }, []);
-  /* useEffect(() => {
-  const mesh = manualSkinnedMesh;
-  if (!mesh) return;
-
-  // Manually trigger matrix updates
-  mesh.updateMatrixWorld(true);
-  mesh.skeleton.bones.forEach((bone) => {
-    bone.updateMatrixWorld(true);
-  });
-
-  // Test: Safely apply a bone transform
-  const target = new THREE.Vector3();
-  try {
-    mesh.applyBoneTransform(0, target);
-    console.log("Initial bone transform (safe):", target);
-  } catch (err) {
-    console.error("applyBoneTransform failed:", err);
-  }
-}, [manualSkinnedMesh]); */
   return (
     manualSkinnedMesh &&
     <group ref={ref}>
