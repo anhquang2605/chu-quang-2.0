efc00b3bcb25599d65be482b32d25cad67c5f091
anhquang2605 <anhquang2605@gmail.com>
2025-07-28 19:04:49 -0700

    removed book UID from key for page

--- components/book/Book.tsx
+++ components/book/Book.tsx
@@ -360,7 +360,7 @@ const Book: React.FC = () => {
         {
           pageList.map((pageD, index) => (
             <Page 
-              key={`${bookUID}-${index}`}
+              key={`${index}`}
               page={page} 
               opened={page > index}  
               number={index} 
