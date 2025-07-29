fd189f3270d3d355f1f8533056858906a150e785
Anh Quang Chu <anhquang2605@gmail.com>
2025-07-29 10:11:39 -0700

    setting book id causing the book to reset its opening stage

--- components/book/Book.tsx
+++ components/book/Book.tsx
@@ -323,8 +323,8 @@ const Book: React.FC = () => {
       const currentPage = tempList.splice(pageNumber, 1);
       //add the current page to the one before the last page
       tempList.splice(tempList.length - 2, 0, currentPage[0]);
-      //setPageList(tempList);
-      setBookUID(generateBookUID());
+      setPageList(tempList);
+      //setBookUID(generateBookUID());
     } else {
       //move the page to the destination
       const tempList = [...pageList];
